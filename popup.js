58ce5677d21e578494d6525e3a4b6f5673121253
Sran012 <88543485+Sran012@users.noreply.github.com>
2025-09-24 14:30:00 +0000

    added cateorised emails finding

--- popup.js
+++ popup.js
@@ -1,45 +1,27 @@
 
 document.getElementById("submit").addEventListener('click', () => {
 
-	/* trying to get emails from textarea and detect emails using regex */
-
-	// const text = document.getElementById("textarea").value;
-	// const emailPattern = /[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
-	// const foundEmails = text.match(emailPattern);
-
-	// if (foundEmails) {
-	// 	const cleanEmails = [];
-	// 	for (let i = 0; i < foundEmails.length; i++) {
-	// 		const email = foundEmails[i].toLowerCase().trim();
-	// 		if (!cleanEmails.includes(email)) {
-	// 			cleanEmails.push(email);
-	// 		}
-	// 	}
-
-	// 	let output = 'Found ' + cleanEmails.length + ' emails :<br>';
-	// 	for (let i = 0; i < cleanEmails.length; i++) {
-	// 		output += cleanEmails[i] + '<br>';
-	// 	}
-	// 	document.getElementById('result').innerHTML = output;
-	// } else {
-	// 	document.getElementById('result').innerHTML = 'No emails found';
-	// }
-
-
-	/* now, getting emails from the web page */
-
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 		chrome.scripting.executeScript({
 			target: {tabId: tabs[0].id},
 			function: getPageText
 		}, function(results){
 			const emails = results[0].result;
-			const resultsDiv = document.getElementById('result');
+			
+			const otherEmails = emails.otherEmails;
+			const supportEmails = emails.supportEmails;
+			const salesEmails = emails.salesEmails;
 
-			if(emails.length === 0){
+			const otherUniqueEmails = [...new Set(otherEmails)];
+			const supportUniqueEmails = [...new Set(supportEmails)];
+			const salesUniqueEmails = [...new Set(salesEmails)];
+
+
+			const resultsDiv = document.getElementById('result');
+			if(otherUniqueEmails.length === 0 && supportUniqueEmails.length === 0 && salesUniqueEmails.length === 0){
 				resultsDiv.innerHTML = "No emails found";
 			} else {
-				resultsDiv.innerHTML = "found : <br>" + emails.join(", ");
+				resultsDiv.innerHTML = "sales : <br>" + salesUniqueEmails.join(", ") + "Support : <br>" + supportUniqueEmails.join(", ") + "<br>Other : <br>" + otherUniqueEmails.join(", ");
 			}
 			}
 		);
@@ -55,5 +37,22 @@ function getPageText() {
 
 	const foundEmails = pageText.match(emailPattern);
 
-	return foundEmails;
+	const supportEmails = [];
+	const salesEmails = [];
+	const otherEmails = [];
+
+	if (foundEmails) {
+		foundEmails.forEach(email => {
+			const firstPart = email.split('@')[0];
+			if (firstPart.includes("support")){
+				supportEmails.push(email.toLowerCase());
+			} else if (firstPart.includes("sales")){
+				sales.push(email.toLowerCase());
+			} else {
+				otherEmails.push(email.toLowerCase());
+			}
+		})
+	}
+
+	return {"supportEmails": supportEmails,"salesEmails" : salesEmails, "otherEmails": otherEmails};
 }
